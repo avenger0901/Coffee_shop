a32bc24fe00416b9f48a3e47c93bc86403b520cd
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-22 11:39:40 -0800

    added pics

--- products/product.js
+++ products/product.js
@@ -1,6 +1,17 @@
 import drinksData from '../data/coffee.js';
-import grandma from '../products/render.js';
+import render from '../products/render.js';
 
+console.log(render);
+const drinkListUl = document.getElementById('drink-lists');
 
+for (let i = 0; i < drinksData.length; i++) {
+    const thisDrink = drinksData[i];
 
+    const drinksElement = render(thisDrink);
+
+    console.log(thisDrink);
+    console.log(drinksElement);
+    drinkListUl.appendChild(drinksElement);
+    
+}
 
--- products/render.js
+++ products/render.js
@@ -1,9 +1,33 @@
 
-export default () =>{
+export default function render(drink){
     console.log('hahahhaha');
     const drinksElement = document.createElement('li');
-    const imageDiv = document.createElement('div');
-    imageDiv.className = 'image-container';
-    drinksElement.appendChild(imageDiv);
+    drinksElement.className = 'drink-container';
+    const imageContainer = document.createElement('div');
+    imageContainer.className = 'image-container';
+    drinksElement.appendChild(imageContainer);
+    const imgTag = document.createElement('img');
+    imgTag.src = drink.image;
+    imgTag.alt = drink.name + ' image';
+    imageContainer.appendChild(imgTag);
+    drinksElement.appendChild(imgTag);
+    const h3Tag = document.createElement('h3');
+    imageContainer.appendChild(h3Tag);
+    h3Tag.textContent = drink.name;
+    const buttonCDiv = document.createElement('div');
+    buttonCDiv.className = 'button-container';
+    const h5Tag = document.createElement('h5');
+    h5Tag.textContent = drink.price;
+    buttonCDiv.appendChild(h5Tag);
+    const buttonTag = document.createElement('button');
+    buttonTag.textContent = 'Add';
+    buttonTag.value = drink.id;
+    buttonCDiv.appendChild(buttonTag);
+    
+
 console.log(drinksElement);
-};
+console.log(buttonCDiv);
+console.log(imgTag);
+    return drinksElement;
+
+}
