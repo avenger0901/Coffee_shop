39f6fc289b544fac88dde869992e54ab7b65c8cb
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-21 17:38:46 -0800

    created design list and product data

---
+++ data/coffee.js
@@ -0,0 +1,73 @@
+const latte = {
+    id:'latte',
+    Name: 'Coffee latte',
+    Type: 'coffee',
+    folavor: 'smoothie and creamy',
+    price: 3.5,
+    cost: 1.5,
+};
+const cappuccino = {
+    id:'cappuccino',
+    Name: 'Coffee cappuccino',
+    Type: 'coffee',
+    folavor: 'foamy and bitter',
+    price: 3.75,
+    cost: 1.35,
+
+};
+const americano = {
+    id: 'americano',
+    Name: 'Americano',
+    Type: 'coffee',
+    folavor: 'bitter',
+    price: 2.75,
+    cost: 1.15,
+
+};
+const dripCoffee = {
+    id:'dripCoffee',
+    Name: 'Regular Coffee',
+    Type: 'coffee',
+    folavor: 'fresh and bitter',
+    price: 2.75,
+    cost: 0.85,
+
+};
+const decafCoffee = {
+    id:'decafCoffee',
+    Name: 'Decaf Coffee',
+    Type: 'decaf coffee',
+    folavor: 'chocolaty and bitter',
+    price: 2.75,
+    cost: 0.85,
+
+};
+const greenTea = {
+    id:'greenTea',
+    Name: 'Green Tea',
+    Type: 'tea',
+    folavor: 'fresh',
+    price: 2.75,
+    cost: 0.95,
+
+};
+const blackTea = {
+    id:'blackTea',
+    Name: 'Black TEa',
+    Type: 'tea',
+    folavor: 'earlgray',
+    price: 2.75,
+    cost: 0.95,
+
+};
+const drinks = [
+    latte,
+    cappuccino,
+    americano,
+    dripCoffee,
+    decafCoffee,
+    greenTea,
+    blackTea
+
+];
+export default drinks;
